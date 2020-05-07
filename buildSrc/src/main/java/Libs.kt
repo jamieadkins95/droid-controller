cfc33b1a582d66ea99c2328f74a02bcdc831e5f4
Jamie Adkins <jamie.adkins@pulselive.com>
2020-05-07 13:18:27 +0100

    Updated with modern gradle setup

---
+++ buildSrc/src/main/java/Libs.kt
@@ -0,0 +1,77 @@
+object Libs {
+    const val gradle_plugin = "com.android.tools.build:gradle:${Versions.gradle_plugin}"
+    const val kotlin_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+
+    //support
+    const val androidx_appcompat = "androidx.appcompat:appcompat:${Versions.androidx_appcompat}"
+    const val androidx_core = "androidx.core:core:${Versions.androidx_core}"
+    const val androidx_constraint_layout = "androidx.constraintlayout:constraintlayout:${Versions.androidx_constraint_layout}"
+    const val material_design = "com.google.android.material:material:${Versions.material_design}"
+    const val nav_component_fragment = "androidx.navigation:navigation-fragment-ktx:${Versions.nav_component}"
+    const val nav_component_ui = "androidx.navigation:navigation-ui-ktx:${Versions.nav_component}"
+    const val work_manager = "androidx.work:work-runtime-ktx:${Versions.work_manager}"
+
+    // Room
+    const val room = "androidx.room:room-runtime:${Versions.room}"
+    const val room_compiler = "androidx.room:room-compiler:${Versions.room}"
+    const val room_ktx = "androidx.room:room-ktx:${Versions.room}"
+    const val room_rx = "androidx.room:room-rxjava2:${Versions.room}"
+
+    //retrofit
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val retrofit_rxjava_adapter = "com.squareup.retrofit2:adapter-rxjava:${Versions.retrofit}"
+    const val retrofit_rxjava2_adapter = "com.jakewharton.retrofit:retrofit2-rxjava2-adapter:1.0.0"
+    const val retrofit_gson_coverter = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+
+    //okhttp
+    const val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
+    const val okio = "com.squareup.okio:okio:${Versions.okio}"
+
+    //rxjava
+    const val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
+    const val rxandroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxandroid}"
+    const val rxpreferences = "com.f2prateek.rx.preferences2:rx-preferences:${Versions.rxpreferences}"
+
+    //Firebase
+    const val firebase_analytics = "com.google.firebase:firebase-analytics:17.2.3"
+    const val firebase_crashlytics = "com.google.firebase:firebase-crashlytics:17.0.0-beta01"
+
+    //dagger
+    const val dagger = "com.google.dagger:dagger:${Versions.dagger}"
+    const val dagger_support = "com.google.dagger:dagger-android-support:${Versions.dagger}"
+    const val dagger_processor = "com.google.dagger:dagger-android-processor:${Versions.dagger}"
+    const val dagger_compiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
+    const val coil = "io.coil-kt:coil:0.9.5"
+
+    const val threetenbp = "org.threeten:threetenbp:${Versions.threetenbp}"
+    const val gson = "com.google.code.gson:gson:${Versions.gson}"
+    const val curl = "com.github.mrmike:ok2curl:${Versions.curl}"
+    const val groupie = "com.xwray:groupie:${Versions.groupie}"
+    const val groupie_extensions = "com.xwray:groupie-kotlin-android-extensions:${Versions.groupie}"
+
+    // Logging
+    const val timber = "com.jakewharton.timber:timber:${Versions.timber}"
+
+    // TEST
+    const val junit = "junit:junit:${Versions.junit}"
+
+    const val kotlin_test_runtime = "org.jetbrains.kotlin:kotlin-test:${Versions.kotlin}"
+    const val kotlin_test_reflect = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
+    const val kotlin_test_annotations = "org.jetbrains.kotlin:kotlin-test-annotations-common:${Versions.kotlin}"
+    const val kotlin_test_junit = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlin}"
+
+    const val mockito = "org.mockito:mockito-core:${Versions.mockito}"
+    const val mockito_android = "org.mockito:mockito-android:${Versions.mockito}"
+    const val mockito_inline = "org.mockito:mockito-inline:${Versions.mockito}"
+    const val mockito_kotlin = "com.nhaarman:mockito-kotlin:${Versions.mockito_kotlin}"
+
+    const val androidx_test_runner = "androidx.test:runner:${Versions.androidx_test_runner}"
+    const val androidx_test_rules = "androidx.test:rules:${Versions.androidx_test_rules}"
+    const val androidx_test_core = "androidx.test:core:${Versions.androidx_test_core}"
+    const val androidx_junit = "androidx.test.ext:junit:${Versions.androidx_junit}"
+    const val androidx_truth = "androidx.test.ext:truth:${Versions.androidx_truth}"
+    const val androidx_espresso_contrib = "androidx.test.espresso:espresso-contrib:${Versions.espresso}"
+    const val androidx_espresso_core = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+    const val androidx_fragment_testing = "androidx.fragment:fragment-testing:${Versions.fragment_test}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,48 @@
+object Versions {
+    const val compile_sdk = 29
+    const val min_sdk = 21
+    const val target_sdk = 29
+
+    const val gradle_plugin = "4.0.0-beta05"
+    const val kotlin = "1.3.61"
+
+    const val app_version_name = "0.5.0"
+    const val app_version_code = 5
+
+    const val androidx_core = "1.2.0"
+    const val androidx_appcompat = "1.1.0"
+    const val androidx_constraint_layout = "2.0.0-beta4"
+    const val material_design = "1.2.0-alpha02"
+    const val nav_component = "2.2.1"
+    const val work_manager = "2.3.4"
+    const val room = "2.2.5"
+
+    const val retrofit = "2.4.0"
+    const val okhttp = "3.8.0"
+    const val okio = "1.13.0"
+    const val rxjava = "2.2.18"
+    const val rxandroid = "2.0.2"
+    const val rxpreferences = "2.0.0"
+    const val gson = "2.8.2"
+    const val curl = "0.3.1"
+    const val dagger = "2.26"
+    const val glide = "4.9.0"
+
+    const val threetenbp = "1.4.2"
+    const val groupie = "2.7.2"
+
+    // Logging
+    const val timber = "4.7.0"
+
+    // TEST
+    const val junit = "4.12"
+    const val mockito = "2.22.0"
+    const val mockito_kotlin = "1.5.0"
+    const val espresso = "3.1.0"
+    const val androidx_test_core = "1.0.0"
+    const val androidx_test_runner = "1.1.0"
+    const val androidx_test_rules = "1.1.0"
+    const val androidx_junit = "1.0.0"
+    const val androidx_truth = "1.1.0"
+    const val fragment_test = "1.1.0-alpha05"
+}
