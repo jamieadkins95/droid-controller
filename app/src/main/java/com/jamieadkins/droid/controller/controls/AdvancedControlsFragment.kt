e6b684128c12883e6f6ba0cfb086ff724a8e6ba7
Jamie Adkins <jamie.adkins@pulselive.com>
2020-05-10 13:53:00 +0100

    First pass at advanced input commands

--- app/src/main/java/com/jamieadkins/droid/controller/controls/AdvancedControlsFragment.kt
+++ app/src/main/java/com/jamieadkins/droid/controller/controls/AdvancedControlsFragment.kt
@@ -5,14 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.jamieadkins.droid.controller.databinding.FragmentAdvancedControlsBinding
 import com.jamieadkins.droid.controller.databinding.FragmentControlsBinding
 
 class AdvancedControlsFragment : BottomSheetDialogFragment() {
 
-    private var binding: FragmentControlsBinding? = null
+    private var binding: FragmentAdvancedControlsBinding? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val newBinding = FragmentControlsBinding.inflate(inflater, container, false)
+        val newBinding = FragmentAdvancedControlsBinding.inflate(inflater, container, false)
         binding = newBinding
         return newBinding.root
     }
